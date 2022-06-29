3549028fa22bbd113bc562028a4328c1945c4963
SanjanaSinghh <sanjana.kolkata13@gmail.com>
2022-06-29 17:14:37 +0530

    Payment

--- src/components/checkout/checkout.js
+++ src/components/checkout/checkout.js
@@ -75,7 +75,7 @@ export default function One() {
                         </Box>
                       
                     </Flex>
-                    <Button borderRadius={25} padding="2.5%" m='8%' colorScheme='blue' onClick={() => nextpage()}>Save & Continue</Button>
+                    <Button width={'300px'} borderRadius={25} padding="2.5%" m='8%' colorScheme='blue' onClick={() => nextpage()}>Save & Continue</Button>
                 </Box>
                 <Box h='fit-content' spacing='50px' padding='2%' paddingTop='2%'  width='-moz-max-content' border='1px' borderColor='grey' borderRadius='10px'>
                     <HStack spacing='50px'>
@@ -138,7 +138,13 @@ export default function One() {
 }
 
 
-
+const my={
+    padding:'8%',
+        position: 'fixed',
+        top:0,
+        width:'40%',
+        right: 1,
+}
 
 
 
@@ -165,13 +171,13 @@ setsai(shah)
             </Center>
             
 <Box w='80%' ml='10%'>
-<Box border={'5px'} borderColor='red'><Box  ml='2%' fontSize='35px' borderBottom='5px'  borderColor='black'> <Text borderBottom='2px'p='20px'mb='25px'w={'400%'} borderColor='Black'>Checkout</Text></Box>
+<Box border={'5px'} borderColor='red'><Box  ml='0%' fontSize='35px' > <Text borderBottom='2px'p='20px'mb='25px'w={'70%'} borderColor='Black'>Checkout</Text></Box>
 {/* <VStack> */}
-<Flex border='1px' borderColor='red' width={'60%'}>
+<Flex width={'70%'}>
     <Heading>
         Shipping Address
     </Heading>
-    <Button ml='200px'>Edit</Button>
+    <Button ml='200px' w='200px'>Edit</Button>
     </Flex>
     <Box textAlign="left" border='10px' borderColor='red'>
         <Text>Your Address</Text>
@@ -183,24 +189,24 @@ setsai(shah)
 {/* <hr/> */}
 <Box mt='30px'>
 
-  <Divider  w='60%'borderColor="black" />
+  <Divider  w='70%'borderColor="black" />
   
 </Box>
 
-<Heading>Delivery & Gift Options</Heading>
+<Heading m='1%' ml='0'>Delivery & Gift Options</Heading>
 
 
 
-<Box backgroundColor={'blackAlpha.50'}>
+<Box mt='1%' mb='2%' backgroundColor={'blackAlpha.50'} w='70%'>
     <Text>We’ve updated our shipping calculations to make them more accurate and
 
 </Text>
 <Text> give you a better idea of when your package will arrive. Got it</Text>
 </Box>
-<HStack border={'2px'}>
+<HStack border={'2px'} w='70%' p='2%'>
 <Radio size={'lg'} colorScheme='blackAlpha'></Radio>
 <Box >
-    <Text fontStyle='' fontWeight={700}>FREE - Standard
+    <Text fontStyle='' fontWeight={600}>FREE - Standard
 </Text>
 
 <Text>Order within 3 hours</Text>
@@ -212,7 +218,7 @@ setsai(shah)
 <Link color={'blue'} fontStyle='Underline'>More delivery Option</Link>
 </Box>
 <Flex border='5px' borderColor='red'>
-<Text  as='Button' onClick={()=>Show()} ml='10%' mt='5% ' fontWeight={600}>Gift other (Option)
+<Text  as='Button' onClick={()=>Show()} ml='10%' mt='2% ' fontWeight={600}>Gift other (Option)
 </Text>
 </Flex>
  <Box ml='10%' display={sai}>
@@ -220,7 +226,7 @@ setsai(shah)
     <Radio size={'lg'} colorScheme='blackAlpha'></Radio>
     <Image w='50px' src='https://www.sephora.com/productimages/sku/s1169937-main-grid.jpg'/>
     </HStack>
-    <Divider  w='60%'borderColor="black"h='10px'/>
+    <Divider  w='70%'borderColor="black"h='10px'/>
 
     <HStack>
     <Radio size={'lg'} colorScheme='blackAlpha'></Radio>
@@ -230,7 +236,7 @@ setsai(shah)
         </Text>
     </Box>
  </HStack>
-    <Divider  w='60%'borderColor="black" h='5px' />
+    <Divider  w='70%'borderColor="black" h='5px' />
 
 
 <Box mt='20px'>
@@ -243,14 +249,67 @@ setsai(shah)
 </Box>
 
 </Box>
-<Box ml='10%' spacing={'10%'} mt='5%' position={'fixed'}>
+<div style={my}>
+<Box h='fit-content' spacing='50px' padding='2%' paddingTop='2%'  width='-moz-max-content' border='1px' borderColor='grey' borderRadius='10px'>
+                    <HStack spacing='50px'>
+                        <Text fontSize='15px'>Merchandise Subtotal</Text>
+                        <Text fontSize='15px' fontFamily='sans-serif' fontWeight='700'>500</Text>
+                    </HStack>
+                    <HStack spacing='50px'><Text fontSize='15px'>Shipping & Handling
+
+                    </Text>
+                        <Text fontSize='15px' fontFamily='sans-serif' fontWeight='700'>Free</Text>
+                    </HStack>
+
+                    <HStack borderBottom='2px' borderColor='Black' paddingBottom='5%' spacing='103px'><Text fontSize='15px'>Estimated Tax
+
+                    </Text>
+                        <Text fontSize='15px' fontFamily='sans-serif' fontWeight='700'>$---</Text>
+                    </HStack>
+                    <HStack mt='3%' spacing='50px'>
+                        <Text fontSize='15px' fontFamily='sans-serif' fontWeight='700'>Estimated TOtal</Text>
+                        <Text fontSize='15px' fontFamily='sans-serif' fontWeight='700' ml='80px'>RS75</Text>
+
+                    </HStack>
+                    <Text fontSize='12px'>or 4 payments of $18.75 with Klarna</Text>
+                    <Divider mt='10px'></Divider>
 
 
-        <Button colorScheme={'blue'} onClick={() => page()}>Save&Pay</Button>
+                    <Box textAlign='end' mb='5px'>
+                        <Text fontSize='9px'>View featured offers</Text>
+                    </Box >
+
+                    <Input
+                        placeholder="Promo or Reward Code"
+                        borderRadius='none'
+                    />
+                    <Divider mt={'10px'}></Divider>
+                    <Box align='center'>
+                        <Button mt='30px' borderRadius={25} w='100%' h='50px'>Place Order</Button>
+                    </Box>
+                    <Box align='center' backgroundColor='gray.200' mt='40px' h='50px' p='15px' borderRadius={5}>
+                        <Text>Item in Order(1)</Text>
+                    </Box>
+                </Box>
+
+                </div>
+<Box ml='10%' spacing={'10%'} mt='2%' >
+
+
+        <Button p='2%' fontSize={'25px'} borderRadius={'15px'} w={'250px'}colorScheme={'blue'} onClick={() => page()}>Save & Pay</Button>
     <Divider  w='60%'borderColor="black" h='5px'mt='5%' mb='5%'/>
-    <Heading>Payment Method</Heading>
+    <Heading color={'grey'}>Payment Method</Heading>
 
-</Box>        </>
+</Box>
+
+<VStack  backgroundColor={'blackAlpha.800'} color='white' mt='70px' padding='3%'><Box> <Text>© 2022 Sephora USA, Inc. All rights reserved.Terms of Use | Privacy Policy </Text>
+</Box>
+   <Box><Text>1-877-737-4672 | TTY: 1-888-866-9845 | customerservice@sephora.com</Text>
+</Box>
+</VStack>
+
+
+</>
         
 
     )
