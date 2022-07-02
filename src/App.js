f72baf329259dfa801551dc70d6d21e9ad6ed454
Sreejith S Kumar <sreejithskumar80@gmail.com>
2022-07-02 14:57:00 +0530

    another commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Description from './pages/Description';
 import One, { Two } from './components/checkout/checkout'
 import Pay from './components/payment/payment'
 import Basket from './components/basket/basket';
+import Footer from "./components/homePage/Footer/Footer";
 
 
 
@@ -41,7 +42,7 @@ function App() {
         <Route path="/ProdDesc/:productId" element={<Description />}></Route>
       </Routes>
 
-
+    <Footer/>
     </div>
   );
 
